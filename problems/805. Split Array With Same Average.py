6b8919975afb15c34ff5895a541b4e9993de2fe8
bswlkh0310 <hhhello0507@gmail.com>
2023-09-19 21:11:02 +0900

    Turns out I couldn't solve it.

--- problems/805. Split Array With Same Average.py
+++ problems/805. Split Array With Same Average.py
@@ -1,29 +1,29 @@
-
 import collections
 
 class Solution(object):
     def go(self, startIdx, flag):
-        checkSum = 0
-        for i in range(self.l):
-            if flag[i]:
-                checkSum += self.arr[i]
-        if self.total / 2 == checkSum:
+        if self.end: return
+        arr1 = [i for (idx, i) in enumerate(self.arr) if flag[idx]]
+        arr2 = [i for (idx, i) in enumerate(self.arr) if not flag[idx]]
+        
+        l1 = len(arr1)
+        l2 = len(arr2)
+
+        if l1 != 0 and l2 != 0 and sum(arr1) / float(l1) == sum(arr2) / float(l2):
             self.end = True
             return
-        
+
         for i in range(startIdx, self.l):
             if not flag[i]:
                 newFlag = flag[:]
                 newFlag[i] = True
                 self.go(i, newFlag)
-        
 
     def splitArraySameAverage(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        self.total = sum(nums)
         self.end = False
         self.arr = nums
         self.l = len(nums)
@@ -35,3 +35,5 @@ class Solution(object):
             self.go(startIdx, newFlag)
 
         return self.end
+
+        
