62b7e5a85c6cdd779f90f1a4d4ba8b282905af06
이강현 <128120228+bestswlkh0310@users.noreply.github.com>
2023-09-21 11:48:05 +0900

    Time: 100 ms (52.06%), Space: 16.5 MB (5.54%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic = {}
+        for i in nums:
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+        
+        for (idx, i) in dic.items():
+            if i != 2:
+                return idx
