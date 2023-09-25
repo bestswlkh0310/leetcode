b1131a7f288633a7d071734b115dba02c85d4524
이강현 <128120228+bestswlkh0310@users.noreply.github.com>
2023-09-25 15:07:20 +0900

    Time: 78 ms (5.01%), Space: 13.6 MB (57.55%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        before = ""
+        for i in range(len(chars) - 1):
+            print(str(type(chars[i])) == "<type 'unicode'>")
+            if before != chars[i] and str(type(chars[i])) == "<type 'unicode'>":
+                before = chars[i]
+            if chars[i] == chars[i + 1]:
+                before = chars[i]
+                chars[i + 1] = 2
+            elif before == chars[i + 1]:
+                chars[i + 1] = chars[i] + 1
+                chars[i] = None
+        for i in range(len(chars) - 1, -1, -1):
+            if chars[i] == None:
+                del chars[i]
+            elif str(chars[i]).isdigit():
+                lst = list(map(str, list(str(chars[i]))))
+                del chars[i]
+                for j in range(len(lst)):
+                    chars.insert(i + j, lst[j])
+        
+        print(chars)
+        return len(chars)
