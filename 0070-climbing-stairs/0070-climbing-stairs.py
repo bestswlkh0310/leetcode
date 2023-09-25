bbf8d7cdbb337969900c07c7e71e763cd08eeba2
이강현 <128120228+bestswlkh0310@users.noreply.github.com>
2023-09-25 12:21:49 +0900

    Time: 15 ms (44.76%), Space: 13.2 MB (45.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -15,8 +15,6 @@ class Solution(object):
         for i in range(1, n):
             for j in range(n):
                 arr[i + 1][j + 1] = arr[i][j] + arr[i][j + 1]
-        for i in arr:
-            print(i)
         j = 0
         for i in range(n + 1, 0, -1):
             cnt += arr[i - 1][i - j - 1]
@@ -24,6 +22,4 @@ class Solution(object):
             if i - j < 0:
                 break
             
-        for i in arr:
-            print(i)
         return cnt
