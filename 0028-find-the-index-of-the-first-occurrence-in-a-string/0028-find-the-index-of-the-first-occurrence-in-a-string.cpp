53c2ce5563b0fb3ca138e427f07e6cf070fa48fb
이강현 <128120228+bestswlkh0310@users.noreply.github.com>
2023-10-03 14:47:12 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (51.00%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         int result = haystack.find(needle);
-        if (result == std::string::npos) {
-            return -1;
-        } else {
-            return result;
-        }
+        return result == std::string::npos ? -1 : result;
     }
 };
