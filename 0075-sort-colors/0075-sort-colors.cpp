d320a8a7c79fcab730aabd164a033704419b8041
이강현 <128120228+bestswlkh0310@users.noreply.github.com>
2023-10-03 14:53:43 +0900

    Time: 2 ms (42.80%), Space: 8.7 MB (10.12%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        std::cout << nums.size();
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+    }
+};
